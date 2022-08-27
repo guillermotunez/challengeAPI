91792f2243539568379933afeb43cded24b89435
Guillermo Túnez <guillermo.tunez@ank.app>
2022-08-27 19:02:19 -0300

    Single Swagger3 Docket.

--- src/main/java/com/challenge/api/ChallengeApiApplication.java
+++ src/main/java/com/challenge/api/ChallengeApiApplication.java
@@ -1,16 +1,7 @@
 package com.challenge.api;
 
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import static org.springdoc.core.Constants.ALL_PATTERN;
 
 @SpringBootApplication
 public class ChallengeApiApplication {
@@ -19,31 +10,4 @@ public class ChallengeApiApplication {
 		SpringApplication.run(ChallengeApiApplication.class, args);
 	}
 
-	@Bean
-	@Profile("!prod")
-	public GroupedOpenApi actuatorApi(//OpenApiCustomiser actuatorOpenApiCustomiser,
-									  //OperationCustomizer actuatorCustomizer,
-									  WebEndpointProperties endpointProperties,
-									  @Value("${springdoc.version}") String appVersion) {
-		return GroupedOpenApi.builder()
-				.group("Actuator")
-				.pathsToMatch(endpointProperties.getBasePath()+ ALL_PATTERN)
-				//.addOpenApiCustomiser(actuatorOpenApiCustomiser)
-				.addOpenApiCustomiser(openApi -> openApi.info(new Info().title("Actuator API").version(appVersion)))
-				//.addOperationCustomizer(actuatorCustomizer)
-				.pathsToExclude("/health/*")
-				.build();
-	}
-
-	@Bean
-	public GroupedOpenApi usersGroup(@Value("${springdoc.version}") String appVersion) {
-		return GroupedOpenApi.builder().group("API")
-				.addOperationCustomizer((operation, handlerMethod) -> {
-					operation.addSecurityItem(new SecurityRequirement().addList("basicScheme"));
-					return operation;
-				})
-				.addOpenApiCustomiser(openApi -> openApi.info(new Info().title("API").version(appVersion)))
-				.packagesToScan("com.challenge.api.controller")
-				.build();
-	}
 }
--- src/main/java/com/challenge/api/config/SecurityConfig.java
+++ src/main/java/com/challenge/api/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.challenge.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,8 +11,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-     security.httpBasic().disable();
+    protected void configure(HttpSecurity security) throws Exception {
+        security.httpBasic().disable();
+//        security.authorizeRequests(authz -> authz
+//                .antMatchers(HttpMethod.GET, "/holaMundo/**").hasAuthority("SCOPE_read")
+//                //.antMatchers(HttpMethod.POST, "/foos").hasAuthority("SCOPE_write")
+//                .anyRequest().authenticated())
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
     }
 }
