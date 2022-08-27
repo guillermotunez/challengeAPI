8b3e89d3a533ebdb0b3afd564abbaa04df8fdcdf
Guillermo Túnez <guillermo.tunez@ank.app>
2022-08-27 15:18:19 -0300

    Add oauth2-resource-server dependency & remove default spring security log in page.

--- src/main/java/com/challenge/api/ChallengeApiApplication.java
+++ src/main/java/com/challenge/api/ChallengeApiApplication.java
@@ -3,8 +3,6 @@ package com.challenge.api;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springdoc.core.GroupedOpenApi;
-import org.springdoc.core.customizers.OpenApiCustomiser;
-import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
